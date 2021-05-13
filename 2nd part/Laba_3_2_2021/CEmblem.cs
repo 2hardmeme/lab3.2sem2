3968363de875f6f103dc19de7ab29de9f79870d3
2hardmeme <58321702+2hardmeme@users.noreply.github.com>
2021-05-13 11:33:56 +0300

    Add files via upload

---
+++ 2nd part/Laba_3_2_2021/CEmblem.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace Laba_3_2_2021
+{
+    class CEmblem
+    {
+        Color color { get; set; }
+        public Color currentColor { get; set; }
+        int size { get; set; }
+        int x { get; set; }
+        int y { get; set; }
+        string name { get; set; }
+
+        Pen pen = new Pen(Color.Blue, 2);
+        PointF rotatePoint;
+        double diagonal;
+
+        public CEmblem(int x, int y, int size, string name, Color color)
+        {
+            this.x = x;
+            this.y = y;
+            this.size = size;
+            this.name = name;
+            this.color = color;
+            currentColor = color;
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
+
+        public void Hide()
+        {
+            currentColor = Color.White;
+        }
+
+        public void Show()
+        {
+            currentColor = color;
+        }
+
+        int x11 = 45;
+        public void Rotate(PictureBox pictureBox)
+        {
+            x11 += 45;
+        }
+
+        public void Draw(PictureBox pictureBox)
+        {
+            Point[] points = new Point[3];
+            pen.Color = currentColor;
+            using (Graphics g1 = pictureBox.CreateGraphics())
+            {
+                rotatePoint = new PointF(x + size / 2, y + size / 2);
+                Matrix myMatrix = new Matrix();
+                myMatrix.RotateAt(45, rotatePoint, MatrixOrder.Append);
+                g1.Transform = myMatrix;
+                g1.DrawRectangle(pen, x, y, size, size);
+            }
+            using (Graphics g1 = pictureBox.CreateGraphics())
+            {
+                points[0].X = x + size / 2; points[0].Y = y;
+                points[1].X = x + size / 15; points[1].Y = y + size / 4 * 3;
+                points[2].X = x + size - (size / 15); points[2].Y = y + size / 4 * 3;
+                g1.DrawPolygon(pen, points);
+
+                g1.DrawEllipse(pen, x, y, size, size);
+            }
+        }
+
+        public void MoveRight()
+        {
+            if(rotatePoint.X < (852-(diagonal*2))-150) x+=4;
+            else MessageBox.Show("вихід за межі");
+        }
+        public void MoveLeft()
+        {
+            if (rotatePoint.X > diagonal+4) x-=4;
+            else MessageBox.Show("вихід за межі");
+        }
+        public void MoveUp()
+        {
+            if (rotatePoint.Y > diagonal + 4) y -= 4;
+            else MessageBox.Show("вихід за межі");
+        }
+        public void MoveDown()
+        {
+            if (y + size + (size - size / 2) < 510) y+=4;
+            else MessageBox.Show("вихід за межі");
+        }
+        
+        public void EndUp()
+        {
+            y = (size - size / 2)/2;
+        }
+        public void EndDown()
+        {
+            y = 495-size- ((size - size / 2) / 2);
+        }
+        public void EndLeft()
+        {
+            x = (size - size / 2) / 2;
+        }
+        public void EndRight()
+        {
+            x = 616 - size;
+        }
+
+        public void Enlarge()
+        {
+            if (rotatePoint.X < (852 - (diagonal * 2)) && rotatePoint.Y < (610 - (diagonal * 2))) size+=4;
+            else MessageBox.Show("сильно велика емблемка виходить!!");
+        }
+        public void Reduce()
+        {
+            if (size > 0) size-=4;
+            else MessageBox.Show("куди менше, вже й так точка!");
+        }
+    }
+}
---
+++ 2nd part/Laba_3_2_2021/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Laba_3_2_2021
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(852, 610);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(908, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(256, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Створити об\'єкт";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Lime;
+            this.button2.Location = new System.Drawing.Point(908, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(256, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Приховати об\'єкт";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Lime;
+            this.button3.Location = new System.Drawing.Point(908, 186);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(256, 40);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Показати об\'єкт";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Honeydew;
+            this.button4.Location = new System.Drawing.Point(1008, 308);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(54, 34);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "↑";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Honeydew;
+            this.button5.Location = new System.Drawing.Point(1008, 268);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(54, 34);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "↑↑";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Honeydew;
+            this.button6.Location = new System.Drawing.Point(953, 371);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(39, 55);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "←";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Honeydew;
+            this.button7.Location = new System.Drawing.Point(908, 371);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(39, 55);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "←←";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Honeydew;
+            this.button8.Location = new System.Drawing.Point(1081, 371);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(39, 55);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "→";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Honeydew;
+            this.button9.Location = new System.Drawing.Point(1126, 371);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(38, 55);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "→→";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Honeydew;
+            this.button10.Location = new System.Drawing.Point(1008, 361);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(54, 36);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "+";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.Honeydew;
+            this.button11.Location = new System.Drawing.Point(1008, 403);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(54, 36);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "-";
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.Honeydew;
+            this.button12.Location = new System.Drawing.Point(1008, 456);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(54, 36);
+            this.button12.TabIndex = 12;
+            this.button12.Text = "↓";
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.Honeydew;
+            this.button13.Location = new System.Drawing.Point(1008, 498);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(54, 36);
+            this.button13.TabIndex = 13;
+            this.button13.Text = "↓↓";
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(905, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 17);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "об`єкти:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(908, 48);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(256, 24);
+            this.comboBox1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleGreen;
+            this.ClientSize = new System.Drawing.Size(1183, 634);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
